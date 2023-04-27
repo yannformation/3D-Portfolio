747f778e8151055d9367ba6336f804dfb6f3d6ba
Yann6666 <yann.cotard01@gmail.com>
2023-04-27 13:22:04 +0200

    Navbar et Hamburger

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,41 @@
+import { BrowserRouter } from "react-router-dom";
+
+import {
+  About,
+  Contact,
+  Experience,
+  Tech,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Works,
+  StarsCanvas,
+} from "./components";
+
+// Structure de notre site
 const App = () => {
   return (
-   <div>
-    3D Developer Portfolio
-   </div>
-  )
-}
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
 
-export default App
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
